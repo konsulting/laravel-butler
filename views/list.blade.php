e444980531c9514a11ddede372af58e2b72d5f88
Keoghan Litchfield <keoghan@klever.co.uk>
2016-11-03 12:01:07 +0000

    Remove 'Or Login with' text from the list view. Make it more reusable.

--- views/list.blade.php
+++ views/list.blade.php
@@ -1,7 +1,6 @@
 @unless(Butler::providers()->isEmpty())
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4 text-center">
-            <h4>Or, login with</h4>
             @foreach (\Butler::providers() as $code => $details)
                 <a href="{{ route('butler.redirect', $code) }}" class="btn btn-default btn-block {{ $details->class }}">
                     @if ($details->icon)
