cbdc427c0a2df01178a76d7e0afdec9c21cc8a1f
Keoghan Litchfield <keoghan@klever.co.uk>
2017-01-28 18:15:58 +0000

    Add index to user_id on social_identities for speeding up searches with lots of records.

--- migrations/2016_11_01_000000_create_social_identities_table.php
+++ migrations/2016_11_01_000000_create_social_identities_table.php
@@ -14,7 +14,7 @@ class CreateSocialIdentitiesTable extends Migration
     {
         Schema::create('social_identities', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->integer('user_id')->unsigned()->index();
             $table->string('provider');
             $table->string('reference');
             $table->text('access_token');
